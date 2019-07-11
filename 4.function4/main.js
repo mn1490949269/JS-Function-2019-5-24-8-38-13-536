1b306898b835fa11bf05d582a93a4cf30e783ba7
mn1490949269 <1490949269@qq.com>
2019-07-11 20:14:19 +0800

    Create main.js

---
+++ 4.function4/main.js
@@ -0,0 +1,8 @@
+function countWords(message){
+  var strs=new Array();
+  strs=str.split(" ");
+  alert(strs.length);
+   
+   // wirte your code here
+}
+countWords('Good morning, I love JavaScript.'); // should return 5
